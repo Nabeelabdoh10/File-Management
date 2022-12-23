f5c2b34b4be28e88338df195692735b07198aafa
mosaabodeh <79007089+mosaabodeh@users.noreply.github.com>
2022-12-23 22:30:33 +0200

    Add files via upload
    write file

---
+++ writeFile.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.io.File;
+import java.sql.*;
+
+
+public class writeFile {
+    public static void createAndWriteToFile(String fileName,StringBuilder data) {
+        try {
+            // Create a new file object
+            File file = new File(fileName);
+            PrintWriter writer = new PrintWriter(file);
+            writer.println(data);
+            // Close the PrintWriter object to ensure the data is written to the file
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void appendAndWriteToFile(String fileName,StringBuilder data) {
+
+        try {
+            // Create a FileWriter and a BufferedWriter
+            FileWriter writer = new FileWriter(fileName, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            bufferedWriter.write(String.valueOf(data));
+            // Close the writers
+            bufferedWriter.close();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void writer(String fileName,StringBuilder Data) throws SQLException, IOException {
+        // Create a File object for the file you want to read
+        File file = new File(fileName);
+
+        Connection conn = null;
+        conn = FileToDatabase.connection();
+        ResultSet rs = null;
+        Statement stmt = null;
+        // Read the contents of the file
+        try {
+            FileReader reader = new FileReader(file);
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT  data  FROM files WHERE name = '" + fileName + "'");
+            byte[] data = new byte[0];
+            if (rs.next()) {
+                Blob fileData = rs.getBlob("data");
+                data = fileData.getBytes(1, (int) fileData.length());
+               // System.out.println("File data: \n" + new String(data));
+            }
+            int c;
+            String oldData=new String(data);
+            // Check if the data is equal to null
+            if (oldData.toString().equals("")) {
+                createAndWriteToFile(fileName,Data);
+            } else {
+                appendAndWriteToFile(fileName,Data);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException, SQLException {
+
+        System.out.print("\nWrite in   file, please enter \"<exit>\" in new line To end writing\n" + "  >>>   ");
+        StringBuilder sb = new StringBuilder();
+        String writeInFile = br.readLine();
+        while( !writeInFile.equals("<exit>") )
+        {
+            sb.append(writeInFile+"\n");
+            writeInFile = br.readLine();
+        }
+        writeFile.writer("Test.txt",sb);
+
+    }
+}
