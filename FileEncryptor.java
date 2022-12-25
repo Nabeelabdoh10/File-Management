5795bb6a1517209f239df6fb26c59721c8e6737f
Nabeelabdoh10 <68351894+Nabeelabdoh10@users.noreply.github.com>
2022-12-25 22:11:29 +0300

    Update FileEncryptor.java

--- FileEncryptor.java
+++ FileEncryptor.java
@@ -1,24 +1,26 @@
-
 public class FileEncryptor {
+     private final int static BIG_INT = 1000000000000000000;
+     private static long randomNumber() {
+    return (long)(Math.random() * BIG_INT);
+  }
+  
 
-        private static long randomNumber() {
-            return (long) (Math.random() * 1000000000000000000L);
-        }
+  public static String encrypt(String filename) {
+     private final static String X = 19923040;
+    return filename + "$" + X;
+  }
 
-        public static String encrypt(String filename) {
-            return filename + "$" + 19923040;
-        }
+  public static String decrypt(String filename) {
+    return filename.substring(0, filename.lastIndexOf("$"));
+  }
 
-        public static String decrypt(String filename) {
-            return filename.substring(0, filename.lastIndexOf("$"));
-        }
 
-        public static void main(String[] args) {
-            String text = "file.txt";
-            String encoded = encrypt(text);
-            String decoded = decrypt(encoded);
-            System.out.println(encoded);
-            System.out.println(decoded);
-        }
+  public static void main(String[] args) {
+    String text = "file.txt";
+    String encoded = encrypt(text);
+    String decoded = decrypt(encoded);
+    System.out.println(encoded);
+    System.out.println(decoded);
+  }
 
 }
