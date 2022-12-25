2bc24604511d1814ad16d3ab2cb205710dd7dde6
mosaabodeh <79007089+mosaabodeh@users.noreply.github.com>
2022-12-25 19:09:55 +0200

    Add files via upload
    rollback

---
+++ RolBacke.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class RolBacke {
+
+    public static void rollBack(String fileName,int version) throws IOException, SQLException {
+        FileToDatabase.deleteFile(fileName , version);
+        System.out.println("previous version for '"+fileName+ "' is : V("+--version+")");
+        FileToDatabase.importFile(fileName,version);
+    }
+
+    public static void main(String[] args) throws SQLException, IOException {
+        rollBack("file10.txt",1);
+    }
+}
