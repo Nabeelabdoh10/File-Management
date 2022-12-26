8fff2fd6341b6f1631e3d93686999bb92675b5ca
mosaabodeh <79007089+mosaabodeh@users.noreply.github.com>
2022-12-26 10:10:08 +0200

    Update Extension.java
    clean code

--- CleanCod/Extension.java
+++ CleanCod/Extension.java
@@ -1,13 +1,16 @@
 package CleanCod;
 
 public class Extension {
+    static final int beginIndex=0;
+    static final char lastIndexOf ='.';
     public static String extension(String path) {
         String extension = "";
 
-        int index = path.lastIndexOf('.');
-        if (index > 0) {
+        int index = path.lastIndexOf(lastIndexOf);
+        if (index > beginIndex) {
             extension = path.substring(index + 1);
         }
         return extension;
     }
+
 }
