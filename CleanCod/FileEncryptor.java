14391fca7829924f59e42c59319acab16e932771
Maymona arar <75255366+MaymonaArar@users.noreply.github.com>
2022-12-26 10:12:44 +0200

    Update FileEncryptor.java

--- CleanCod/FileEncryptor.java
+++ CleanCod/FileEncryptor.java
@@ -3,14 +3,15 @@ package CleanCod;
 public class FileEncryptor {
 
         
-static  final  int randomNumber=19923040;
-static final char specialCharatar='$';
+      static  final  int randomNumber=19923040;
+      static final char specialCharatar='$';
+      static int startIndex =0;
     public static String encrypt(String filename) {
         return filename + specialCharatar + randomNumber;
     }
 
     public static String decrypt(String filename) {
-        return filename.substring(0, filename.lastIndexOf(specialCharatar));
+        return filename.substring(startIndex, filename.lastIndexOf(specialCharatar));
     }
 
 }
