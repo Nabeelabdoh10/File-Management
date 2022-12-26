64e2e5eaf4f0580ce87c1e80e078e2b18b2fe910
mosaabodeh <79007089+mosaabodeh@users.noreply.github.com>
2022-12-26 09:54:19 +0200

    Update FileEncryptor.java
    CLEANCODE

--- CleanCod/FileEncryptor.java
+++ CleanCod/FileEncryptor.java
@@ -2,13 +2,15 @@ package CleanCod;
 
 public class FileEncryptor {
 
-        public static String encrypt(String filename) {
-            return filename + "$" + 19923040;
-        }
+        
+static  final  int randomNumber=19923040;
+static final char specialCharatar='$';
+    public static String encrypt(String filename) {
+        return filename + specialCharatar + randomNumber;
+    }
 
-        public static String decrypt(String filename) {
-            return filename.substring(0, filename.lastIndexOf("$"));
-        }
-
+    public static String decrypt(String filename) {
+        return filename.substring(0, filename.lastIndexOf(specialCharatar));
+    }
 
 }
